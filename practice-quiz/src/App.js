01677b0a8a2c281490945f4c2561d40564a118a1
tungnm <kakal0tdz04@gmail.com>
2025-03-22 21:15:16 +0700

    create antdesign

--- practice-quiz/src/App.js
+++ practice-quiz/src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Button, Space } from 'antd';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Space direction="vertical" size="large">
+          <h1>Welcome to React with Ant Design</h1>
+          <Button type="primary">Click me!</Button>
+        </Space>
       </header>
     </div>
   );
